461d50c8127d1ae91291c3fbe20fcd7ff8bd430d
Giacomo Fenzi <giacomofenzi@outlook.com>
2022-06-06 16:52:45 +0200

    Approximate 1b on my machien

--- cpp/src/utils/busy_wait.cpp
+++ cpp/src/utils/busy_wait.cpp
@@ -1,9 +1,7 @@
 #include "busy_wait.hpp"
 
-void busy_wait_cycles(uint64_t cycles) {
-    busy_wait(cycles >> 2);
-}
 
+// TODO: Allow for some calibration
 void busy_wait(uint64_t iterations) {
     for (uint64_t i = 0; i < iterations; i++) {
         __asm__ volatile("" : "+g" (i) : :);
--- cpp/src/utils/busy_wait.hpp
+++ cpp/src/utils/busy_wait.hpp
@@ -1,7 +1,6 @@
 #include <stdint.h>
 
-// Should be relatively accurate but hey what do I know
-void busy_wait_cycles(uint64_t cycles);
 
-// Around 4 cycles per iteration according to our assembly
+#define BILLION_CYCLES (1300000000)
+
 void __attribute__ ((noinline)) busy_wait(uint64_t iterations);
